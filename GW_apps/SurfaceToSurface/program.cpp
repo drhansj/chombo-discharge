139841a897404838e9afdc8014878fee1db89092
Nate Overton-Katz <noverton22@gmail.com>
2024-09-05 11:47:18 -0700

    Trying to hack in voltage diff

--- GW_apps/SurfaceToSurface/program.cpp
+++ GW_apps/SurfaceToSurface/program.cpp
@@ -28,20 +28,49 @@ int main(int argc, char* argv[]){
   auto timestepper = RefCountedPtr<FieldStepper<FieldSolverMultigrid> >
      (new FieldStepper<FieldSolverMultigrid>());
 
-  Real sigma0 = 0.0;
-  Real sigma1 = 10.0;
-  auto sigma = [&](const RealVect& x) -> Real {
-    if (x[0] < 0)
-      return sigma0;
-    else
-      return sigma1;
-  };
+  // Real sigma0 = 0.0;
+  // Real sigma1 = 10.0;
+  // auto sigma = [&](const RealVect& x) -> Real {
+  //   if (x[0] < 0)
+  //     return sigma0;
+  //   else
+  //     return sigma1;
+  // };
 
-  timestepper->setSigma(sigma);
-  
+  // timestepper->setSigma(sigma);
+
   // Set up the Driver and run it
   RefCountedPtr<Driver> engine = RefCountedPtr<Driver> (new Driver(compgeom, timestepper, amr, tagger, geocoarsen));
-  engine->setupAndRun(input_file);
+  //engine->setupAndRun(input_file);
+
+  int initialRegrids, restartStep;
+  pp.get("initial_regrids", initialRegrids);
+  pp.get("restart", restartStep);
+  std::string outputDirectory, outputFileNames;
+  pp.get("output_directory", outputDirectory);
+  pp.get("output_names", outputFileNames);
+  char iter_str[100];
+  sprintf(iter_str, ".check%07d.%dd.hdf5", 0, SpaceDim);
+  const std::string restartFile = outputDirectory + "/chk/" + outputFileNames + std::string(iter_str);
+  engine->setup(input_file, initialRegrids, false, restartFile);
+
+  Real volt0 = 0.0;
+  Real volt1 = 10.0;
+  auto myElectrodeVoltage = [&](const RealVect a_pos, const Real a_time) -> Real{
+    if (a_pos[0] < 0.1)
+      return volt0;
+    else
+      return volt1;
+  };
+  auto fieldsolver = timestepper->getSolver();
+  fieldsolver->setElectrodeDirichletFunction(0, myElectrodeVoltage);
+
+  int maxSteps;
+  Real startTime, stopTime;
+  pp.get("max_steps", maxSteps);
+  pp.get("start_time", startTime);
+  pp.get("stop_time", stopTime);
+  engine->run(startTime, stopTime, maxSteps);
 
 #ifdef CH_MPI
   CH_TIMER_REPORT();
